e3f4ac686beffcf9ff8e4205a01042b519ff51f0
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-07-20 19:48:57 +0700

    create navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg bg-body-white" id="navbar">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/images/logo.svg" alt="" class="img-fluid">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse text-center py-3 py-md-0" id="navbarSupportedContent">
+      <ul class="navbar-nav text-center mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Produk</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Promo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+      <a class="button-primary" href="#">Download App</a>
+    </div>
+  </div>
+</nav>
